038626332abe7722976dd888d890abcdd84ae9c8
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-06-23 16:07:57 -0500

    Navbar Base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Home</div>
+  return <div className='text-stone-100 font-bold text-lg'>Home</div>
 }
---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { buttonVariants } from '../ui/Button'
+
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-800 border-b border-zinc-600 z-[10] py-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        {/* Logo  */}
+        <Link href="/" className="flex items-center gap-2">
+          <Image 
+            src='/branding/Dreadit.png'
+            alt='Dreadit' 
+            width={50}
+            height={50}
+            className='bg-rose-900 rounded-full'
+          />
+
+          <p className="hidden md:block text-2xl font-bold text-rose-000">dreadit</p>
+        </Link>
+
+        {/* Subreddit Dropdown Menu  */}
+
+        {/* Search Bar  */}
+
+        {/* Button Bar  */}
+
+        {/* Login  */}
+        <Link href="/login" className={buttonVariants()}>Login</Link>
+        {/* Account Dropdown Menu */}
+      </div>      
+    </div>
+  )
+}
+
+export default Navbar
+
